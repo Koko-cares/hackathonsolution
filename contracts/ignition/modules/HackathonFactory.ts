f60eb5663845f70fb61ca7e1035e6ddd253e5abf
collinsadi <collinsadi20@gmail.com>
2024-12-04 09:41:16 +0100

    moved project from hardhat to foundry

--- contracts/ignition/modules/HackathonFactory.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const HackathonFactoryModule = buildModule("HackathonFactoryModule", (m) => {
-  const hackathonFactory = m.contract("HackathonFactory");
-
-  return { hackathonFactory };
-});
-
-export default HackathonFactoryModule;
