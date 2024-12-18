c78cd37d0940723feb4b302173caab6e6ef287a7
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-12-18 10:49:01 +0300

    feat: implement navbar component and update global styles for improved layout

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -17,7 +19,12 @@ export default {
         secondaryButton: "#e6f2ff",
         primaryButton: "##000000",
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
