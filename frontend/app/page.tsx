98862fdd5c477f5c996462509748b017f30e93de
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-12-21 12:00:07 +0300

    feat: add Works component to showcase platform functionality and integrate into homepage layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,23 @@
 import Features from "@/components/home/Features";
 import Hero from "@/components/home/Hero";
 import Navbar from "@/components/home/Navbar";
+import Works from "@/components/home/Works";
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-      <Navbar />
-      <Hero />
-      <Features />
-    </div>
+    <>
+      <div className="hidden md:block font-[family-name:var(--font-geist-sans)]">
+        <Navbar />
+        <Hero />
+        <Features />
+        <Works />
+      </div>
+      <div className="flex md:hidden justify-center items-center mt-16 font-[family-name:var(--font-montserrat)] h-screen">
+        <h1 className="text-[30px] text-black font-semibold text-center">
+          {" "}
+          Not Available on Mobile
+        </h1>
+      </div>
+    </>
   );
 }
