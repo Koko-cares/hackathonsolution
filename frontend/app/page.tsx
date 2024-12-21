e3ae6e9cb25fa8caee212af418667c062631898c
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2024-12-21 13:22:53 +0300

    feat: add Footer component to enhance website layout and provide additional information

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,6 @@
 import CTA from "@/components/home/CTA";
 import Features from "@/components/home/Features";
+import Footer from "@/components/home/Footer";
 import Hero from "@/components/home/Hero";
 import Navbar from "@/components/home/Navbar";
 import Works from "@/components/home/Works";
@@ -13,6 +14,7 @@ export default function Home() {
         <Features />
         <Works />
         <CTA />
+        <Footer />
       </div>
       <div className="flex md:hidden justify-center items-center mt-16 font-[family-name:var(--font-montserrat)] h-screen">
         <h1 className="text-[30px] text-black font-semibold text-center">
